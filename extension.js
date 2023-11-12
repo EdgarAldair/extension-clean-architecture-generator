2a53b4a7196649e51da93bc41a46ac42cd0b41f5
Edgar Saenz <edgarsaenz243@gmail.com>
2023-11-12 13:54:18 -0600

    fix: model error creation

--- extension.js
+++ extension.js
@@ -61,7 +61,87 @@ function transformarNombreModeloCamelCase(nombreModelo) {
     return nombreTransformadoACamelCase;
 }
 
+function generateDartFileContent(nombreModelo, campos) {
+  const nombreModeloConMayuscula = nombreModelo.charAt(0).toUpperCase() + nombreModelo.slice(1).toLowerCase();
 
+  // Contenido del archivo Dart
+  let content = `import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../core/domain/utils.dart';
+
+part '${nombreModelo}.g.dart';
+
+@JsonSerializable()
+class ${nombreModeloConMayuscula} extends Equatable {
+const ${nombreModeloConMayuscula}({
+  ${campos.map(campo => `${campo.required ? 'required ' : ''}this.${campo.nombre},`).join('\n    ')}
+});
+
+factory ${nombreModeloConMayuscula}.fromJson(Map<dynamic, dynamic>? json) {
+  return ${nombreModeloConMayuscula}(
+    ${campos.map(campo => `${campo.nombre}: ${generarValorPorTipo(campo)},`).join('\n      ')}
+  );
+}
+
+${campos.map(campo => `final ${campo.tipo === 'string' ? 'String' : campo.tipo}${campo.required ? '' : '?'} ${campo.nombre};`).join('\n\n  ')}
+
+@override
+List<Object?> get props {
+  return [
+    ${campos.map(campo => campo.nombre).join(',\n      ')}
+  ];
+}
+
+Json toJson() => _$${nombreModeloConMayuscula}ToJson(this);
+}
+`;
+
+  return content;
+}
+
+function generarValorPorTipo(campo) {
+  switch (campo.tipo) {
+      case 'string':
+          return `json?['${campo.nombre}'] as String? ?? ${campo.required ? "''" : "''"} `;
+      case 'int':
+          return `json?['${campo.nombre}'] as int? ?? ${campo.required ? '0' : '0'} `;
+      case 'bool':
+          return `json?['${campo.nombre}'] as bool? ?? ${campo.required ? 'false' : 'false'} `;
+      case 'datetime':
+          return `json?['${campo.nombre}'] as DateTime? ?? ${campo.required ? 'DateTime.now()' : ' DateTime.now()'}`;
+      // Agrega más casos según sea necesario
+      default:
+          return `${campo.tipo}? ?? ${campo.required ? "''" : "''"} `;
+  }
+
+}
+
+async function solicitarInformacionCampo() {
+  const nombreCampo = await vscode.window.showInputBox({ prompt: 'Nombre del campo' });
+
+  // Verifica si el usuario canceló la entrada o no proporcionó un nombre
+  if (nombreCampo === undefined || nombreCampo.trim() === '') {
+      vscode.window.showErrorMessage('Nombre del campo inválido o vacío.');
+      return;
+  }
+
+  const esRequerido = await vscode.window.showQuickPick(['Sí', 'No'], { placeHolder: '¿Es requerido?' }) === 'Sí';
+
+  const tipoCampo = await vscode.window.showQuickPick(['String', 'int', 'bool', 'DateTime'], { placeHolder: 'Seleccione el tipo de campo' });
+
+  // Verifica si el usuario canceló la entrada o no proporcionó un tipo de campo
+  if (tipoCampo === undefined) {
+      vscode.window.showErrorMessage('Tipo de campo inválido o no seleccionado.');
+      return;
+  }
+
+  return {
+      nombre: nombreCampo,
+      tipo: tipoCampo.toLowerCase(), // Se utiliza toLowerCase() para garantizar la consistencia
+      required: esRequerido,
+  };
+}
 
 function generateBlocFileContent(nombreModelo) {
     const nombreModeloLaPrimeraMayuscula = transformarNombreModelo(nombreModelo);
@@ -132,44 +212,6 @@ class ${nombreModeloLaPrimeraMayuscula}State with _$${nombreModeloLaPrimeraMayus
 
   factory ${nombreModeloLaPrimeraMayuscula}State.initial() =>
       const ${nombreModeloLaPrimeraMayuscula}State(isLoading: false);
-}
-`;
-
-    return content;
-}
-
-function generateDartFileContent(nombreModelo, campos) {
-    const nombreModeloConMayuscula =transformarNombreModelo(nombreModelo);
-
-    let content = `import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
-
-import '../../core/domain/utils.dart';
-
-part '${nombreModelo}.g.dart';
-
-@JsonSerializable()
-class ${nombreModeloConMayuscula} extends Equatable {
-  const ${nombreModeloConMayuscula}({
-    ${campos.map(campo => `${campo.required ? 'required ' : ''}this.${campo.nombre},`).join('\n    ')}
-  });
-
-  factory ${nombreModeloConMayuscula}.fromJson(Map<dynamic, dynamic>? json) {
-    return ${nombreModeloConMayuscula}(
-      ${campos.map(campo => `${campo.nombre}: ${generarValorPorTipo(campo)},`).join('\n      ')}
-    );
-  }
-
-  ${campos.map(campo => `final ${campo.tipo === 'string' ? 'String' : campo.tipo}${campo.required ? '' : '?'} ${campo.nombre};`).join('\n\n  ')}
-
-  @override
-  List<Object?> get props {
-    return [
-      ${campos.map(campo => campo.nombre).join(',\n      ')}
-    ];
-  }
-
-  Json toJson() => _$${nombreModeloConMayuscula}ToJson(this);
 }
 `;
 
@@ -257,46 +299,6 @@ class ${nombreModeloConMayuscula}FacadeImpl extends I${nombreModeloConMayuscula}
 `;
 
     return content;
-}
-
-
-
-function generarValorPorTipo(campo) {
-    switch (campo.tipo) {
-        case 'string':
-            return `json?['${campo.nombre}'] as String? ?? ${campo.required ? "''" : "''"} `;
-        case 'int':
-            return `json?['${campo.nombre}'] as int? ?? ${campo.required ? '0' : '0'} `;
-        case 'bool':
-            return `json?['${campo.nombre}'] as bool? ?? ${campo.required ? 'false' : 'false'} `;
-        case 'datetime':
-            return `json?['${campo.nombre}'] != null ? DateTime.parse(json['${campo.nombre}']) : ${campo.required ? 'DateTime.now()' : ' DateTime.now()'}`;
-        default:
-            return `${campo.tipo}? ?? ${campo.required ? "''" : "''"} `;
-    }
-}
-
-async function solicitarInformacionCampo() {
-    const nombreCampo = await vscode.window.showInputBox({ prompt: 'Nombre del campo' });
-
-    if (nombreCampo === undefined || nombreCampo.trim() === '') {
-        vscode.window.showErrorMessage('Nombre del campo inválido o vacío.');
-        return;
-    }
-
-    const esRequerido = await vscode.window.showQuickPick(['Sí', 'No'], { placeHolder: '¿Es requerido?' }) === 'Sí';
-
-    const tipoCampo = await vscode.window.showQuickPick(['String', 'int', 'bool', 'DateTime'], { placeHolder: 'Seleccione el tipo de campo' });
-
-    if (tipoCampo === undefined) {
-        vscode.window.showErrorMessage('Tipo de campo inválido o no seleccionado.');
-        return;
-    }
-
-    return {
-        nombre: nombreCampo,
-        required: esRequerido,
-    };
 }
 
 async function activate(context) {
